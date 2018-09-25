3427004529b7ac4f28cdc49b65d46dfefbbc6c13
thiaw_mamadou <mamadou.thiaw@etu.unice.fr>
2018-09-26 00:16:59 +0200

    definition de la methode index() de l'api rest et finition de GET pour le domaine User

--- grails-app/controllers/fr/mbds/webservice/rest/ApiController.groovy
+++ grails-app/controllers/fr/mbds/webservice/rest/ApiController.groovy
@@ -1,20 +1,64 @@
 package fr.mbds.webservice.rest
 
+import fr.mbds.grails.fr.mbds.grails.springsec.User
+import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
+import org.apache.tomcat.jni.Library
+
+import java.awt.print.Book
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
 
 @Secured('permitAll')
 class ApiController {
+    def springSecurityService
 
-    def index() {
-        switch (request.getMethod()){
-            case "POST":
-                render "POST"
-                break
-            case "GET":
-                render"hello"
-                break
-        }
+    @Secured("ROLE_ADMIN")
+    def success() {
+        render(status: 200, text: "Connecté! <br>"
+                + "Bienvenue l'utilisateur " + springSecurityService.currentUser.id + " <br>" +
+                "<a href='/api/index'>aller à l'accueil</a>")
     }
 
+    @Secured("ROLE_ADMIN")
+    def index() {
+
+        render("Bienvenue, voici les web services disponibles: <br>" +
+                "<ol>" +
+                "<li><a href='http://localhost:8080/grails/api/user'>Les Users</a></li>" +
+                "<li><a href='http://localhost:8080/grails/api/message'>Les Messages</a></li>" +
+                "</ol>")
+    }
+
+    def user() {
+        switch (request.getMethod()) {
+            case 'GET':
+                println springSecurityService.getPrincipal().authorities
+                if (!params.id) {
+                    if (User.findAll().size() == 0) {
+                        render(status: 200, text: "Pas de user")
+                    }
+                    def users = [users: User.findAll()]
+                    render users as JSON
+
+                } else {
+                    if (!User.findById(params.id)) {
+                        render(status: 404, text: "User introuvable")
+                        return
+                    }
+                    if (User.findById(params.id)) {
+                        response.status = 200
+                        render User.findById(params.id) as JSON
+                    } else {
+                        response.status = 400
+                    }
+                }
+                break
+     
+            default:
+                response.status = 405
+        }
+    }
 
 }
